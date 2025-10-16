03e46c1e53d122cfd56e9928719998cc5ffc3454
quentcha <quentcha05@gmail.com>
2025-10-16 14:19:47 +0200

    fonction coupe bande

--- TP5_MA312.py
+++ TP5_MA312.py
@@ -22,7 +22,6 @@ plt.show()
 sd.play(data, fe)
 time.sleep(len(data) / fe)#permet au programme d’attendre la fin de la lecture du son avant de se terminer
 sd.stop()
-'''
 ################################## Q.2
 def f(amplitude,t,freq):
     return amplitude*np.sin(2*np.pi*t*freq)
@@ -43,4 +42,36 @@ sd.play(signal, fe)
 time.sleep(len(signal) / fe)#permet au programme d’attendre la fin de la lecture du son avant de se terminer
 sd.stop()
 
+######################Q.5
+fe, data= wavfile.read('guitare1.wav')
+data = data.astype(np.float32)
+if data.ndim == 2 : #stéréo -> mono si besoin
+    data = data.mean(axis =1)
+#data /= (np.max(np.abs(data)) + 1*10**(-12))#normalisation
+#plt.plot(data)
+#plt.show()
+data = np.block([data, np.zeros(2**(int(np.log2(len(data)))+1)-len(data))])
+print(np.log2(len(data)))
 
+spectre = np.fft.rfft(data)
+freq=np.fft.rfftfreq(data.size, d=1./fe )
+spectre_abs=np.abs(spectre)
+plt.plot(freq, spectre_abs)
+plt.xlabel("Frequence , Hz " )
+plt.ylabel("Amplitude")
+plt.show()
+'''
+###############Q.4.5
+fe, data= wavfile.read('guitare1.wav')
+data = data.astype(np.float32)
+if data.ndim == 2 : #stéréo -> mono si besoin
+    data = data.mean(axis =1)
+#data /= (np.max(np.abs(data)) + 1*10**(-12))#normalisation
+
+data = np.block([data, np.zeros(2**(int(np.log2(len(data)))+1)-len(data))])
+fmin,fmax=300,1000
+spectre = np.fft.rfft(data)[fmin:fmax]
+plt.plot(np.abs(spectre))
+plt.xlabel("Frequence , Hz " )
+plt.ylabel("Amplitude")
+plt.show()
