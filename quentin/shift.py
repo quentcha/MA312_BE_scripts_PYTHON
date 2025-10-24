7dbb92a589c27c7864000bfa81f06f3a661ce066
quentcha <quentcha05@gmail.com>
2025-10-24 16:34:15 +0200

    correction pitch

--- quentin/shift.py
+++ quentin/shift.py
@@ -1,6 +1,5 @@
 import numpy as np
 import play
-#rien ne dit qu'on peux pas juste augmenter tout de +1000Hz
 def pitch(data,shift,fe):##erreur va s'additionner car on arrondi et perte des hautes fréquences
     spectre = np.fft.rfft(data)
     freq=np.fft.rfftfreq(data.size, d=1./fe)
@@ -25,13 +24,11 @@ if data.ndim == 2 : #stéréo -> mono si besoin
 data = np.block([data, np.zeros(2**(int(np.log2(len(data)))+1)-len(data))])
 #play.sound(data,fe)
 data_pitched=pitch(data,500,fe)
+freq_pitched=np.fft.rfftfreq(data.size, d=1./fe)
 #play.sound(data_pitched,fe)
-print(data_pitched)
 print("plotting...")
-print(np.abs(np.fft.rfft(data_pitched)))
-
-plt.plot(np.abs(np.fft.rfft(data)))
-plt.plot(np.abs(np.fft.rfft(data_pitched)))
+plt.plot(freq_pitched,np.abs(np.fft.rfft(data)))
+plt.plot(freq_pitched,np.abs(np.fft.rfft(data_pitched)))
 
 plt.xlabel("Frequence , Hz " )
 plt.ylabel("Amplitude")
@@ -43,21 +40,3 @@ plt.show()
 #play.sound(data,fe)
 #play.sound(data_pitched,fe)
 #wavfile.write("example.wav", fe, data_pitched)
-'''
-def pitch(data,n_octave):#pitch shift pur
-    #freq*2 pour +1 octave
-    spectre = np.fft.rfft(data)
-    spectre_pitch=np.zeros(len(spectre), dtype=complex)
-    i_max=int(len(spectre)/(2**n_octave))
-    for i in range(len(spectre)):
-        index=int(i/(2**n_octave))
-        if index < len(spectre):
-            spectre_pitch[i]=spectre[index]
-    plt.plot(np.abs(np.fft.rfft(spectre_pitch)))
-    plt.plot(np.abs(np.fft.rfft(spectre)))
-    plt.xlabel("Frequence , Hz " )
-    plt.ylabel("Amplitude")
-    plt.show()
-    inverse=np.fft.irfft(spectre_pitch)
-    return inverse
-'''
